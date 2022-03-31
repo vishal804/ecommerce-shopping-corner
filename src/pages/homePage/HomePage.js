8186403cbc11f95e51ad8a3988700f8cecb69b4d
vishal804 <vishal804>
2022-03-31 14:46:04 +0530

    Link For Product Page

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
     <>
     <div>HomePage Btween</div>
+    <Link to="/product">
+    <button className='btn'>Product Page</button>
+    </Link>
+    
     </>
   )
 }
