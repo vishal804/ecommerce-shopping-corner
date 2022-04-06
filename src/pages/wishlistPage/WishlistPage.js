13e3bc17a8523f72bc2a4b8477251b19f9e16bff
vishal804 <vishal804>
2022-04-06 12:45:51 +0530

    refactor spacing

--- src/pages/wishlistPage/WishlistPage.js
+++ src/pages/wishlistPage/WishlistPage.js
@@ -19,7 +19,6 @@ const WishlistPage = () => {
                 priceAfterDiscount,
                 originalPrice,
                 discount,
-
                 Image,
                 alt,
               } = product;
