b06f3ab89641bebee6d8b1c9cc9d9111a5fdd91e
vishal804 <vishal804>
2022-08-18 11:31:42 +0530

    footer bugs

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -10,47 +10,10 @@ const Footer = () => {
             <Link to="/">Shoplify</Link>
           </div>
           <p>You Shop Fast We Ship Faster</p>
-          <div className="links-handle flex">
-            <a
-              href="https://www.facebook.com/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <i className="fab fa-facebook fa-2x"></i>
-            </a>
-            <a
-              href="https://www.instagram.com/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <i className="fab fa-instagram fa-2x"></i>
-            </a>
-            <a href="https://twitter.com/" rel="noreferrer" target="_blank">
-              <i className="fab fa-twitter fa-2x"></i>
-            </a>
-            <a
-              href="https://accounts.snapchat.com/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              <i className="fab fa-snapchat fa-2x"></i>
-            </a>
-          </div>
         </div>
 
-        <div className="flex flex-coloum no-show">
-          <div className="item-title">Contact Info</div>
-          <ul className="quick-links">
-            <li>
-              <p>Location</p>
-            </li>
-            <li>
-              <p>+91 999999999</p>
-            </li>
-            <li>
-              <p>shoplify@mail.com</p>
-            </li>
-          </ul>
+        <div className="flex flex-coloum left-footer no-show">
+          <p>© All Rights Reserved @ Shoplify</p>
         </div>
       </footer>
     </>
